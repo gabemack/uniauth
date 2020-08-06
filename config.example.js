183581a13c9705c44ce1a323c2d3c52e85f6ebe1
Gabriel Mack <gabe@gabemack.com>
2020-08-05 23:53:34 -0400

    Added an example config file

---
+++ config.example.js
@@ -0,0 +1,23 @@
+const config = {
+    PORT: 8080,
+    SESSION_KEY: "",
+    DISCORD_OAUTH_URI: "",
+    DISCORD_CLIENT_ID: "",
+    DISCORD_CLIENT_SECRET: "",
+    DISCORD_GRANT_TYPE: "authorization_code",
+    DISCORD_REDIRECT_URI: "URI + /oauth/redirect",
+    DISCORD_SCOPE: "identify guilds.join",
+    DISCORD_ROLES: [""],
+    DISCORD_GUILD_ID: "",
+    DISCORD_BOT_TOKEN: "",
+    GOOGLE_CLIENT_ID: "",
+    GOOGLE_CLIENT_SECRET: "",
+    GOOGLE_GRANT_TYPE: "authorization_code",
+    GOOGLE_PROMPT: "consent",
+    GOOGLE_REDIRECT_URI: "URI + /google/redirect",
+    GOOGLE_EMAIL_DOMAIN: "organization.com",
+    GOOGLE_RESPONSE_TYPE: "code",
+    GOOGLE_SCOPE: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"
+}
+
+module.exports = config;
